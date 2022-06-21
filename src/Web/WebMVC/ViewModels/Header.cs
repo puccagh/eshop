00a04adea17fe34d803f4dc840e56497df81b94b
mx <sasiphat.kg@azurelabs.io>
2022-06-21 15:02:12 +0700

    update

--- src/Web/WebMVC/ViewModels/Header.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Microsoft.eShopOnContainers.WebMVC.ViewModels
-{
-    public class Header
-    {
-        public string Controller { get; set; }
-        public string Text { get; set; }
-    }
-}
